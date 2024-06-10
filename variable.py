d60ca079c5fc6ee9cbe88eb857c45dcc33c7a3b4
Evgeny <117734025+JeffreyWillims@users.noreply.github.com>
2024-06-10 14:05:49 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,7 @@
+
+first_name = 12
+second_name = 1.5
+name = 'Python'
+second_count = second_name / first_name
+
+print(f"Курс: {name}, всего задач:{first_name}, затрачено часов: {second_name}, среднее время выполнения {second_count} часа.")

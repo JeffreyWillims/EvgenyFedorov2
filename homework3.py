3a5ac31debe5209337a4ac1ad288283bf25c7d68
Evgeny <117734025+JeffreyWillims@users.noreply.github.com>
2024-06-10 18:15:32 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,16 @@
+name = "Evgeny"
+print('Name:' + name)
+
+age = 26
+print('Age:', age)
+
+age = 26
+new_age = 4
+age=age + new_age
+print('New Age:', age)
+
+is_student = True
+print('Is Student:', is_student)
+
+
+
